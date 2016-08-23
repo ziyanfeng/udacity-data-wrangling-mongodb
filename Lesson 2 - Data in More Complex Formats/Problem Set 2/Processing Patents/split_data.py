afb5f46397a61103a6aac30596f42cfa30a4dd29
ziyanfeng <ziyanfeng.c@gmail.com>
2016-08-22 22:00:02 -0400

    feat: add solution to `Quiz: Processing Patents`

---
+++ Lesson 2 - Data in More Complex Formats/Problem Set 2/Processing Patents/split_data.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# So, the problem is that the gigantic file is actually not a valid XML, because
+# it has several root elements, and XML declarations.
+# It is, a matter of fact, a collection of a lot of concatenated XML documents.
+# So, one solution would be to split the file into separate documents,
+# so that you can process the resulting files as valid XML documents.
+
+import xml.etree.ElementTree as ET
+
+PATENTS = 'patent.data'
+
+
+def get_root(fname):
+    tree = ET.parse(fname)
+    return tree.getroot()
+
+
+def split_file(filename):
+    """
+    Split the input file into separate files, each containing a single patent.
+    As a hint - each patent declaration starts with the same line that was
+    causing the error found in the previous exercises.
+
+    The new files should be saved with filename in the following format:
+    "{}-{}".format(filename, n) where n is a counter, starting from 0.
+    """
+    with open(filename, 'r') as reader:
+        filenum = -1
+        writer = None
+        for line in reader:
+            if line.find('<?xml version="1.0" encoding="UTF-8"?>') != -1:
+                if writer:
+                    writer.close()
+                filenum += 1
+                writer = open("{}-{}".format(filename, filenum), 'w')
+            writer.write(line)
+        writer.close()
+
+
+def test():
+    split_file(PATENTS)
+    for n in range(4):
+        try:
+            fname = "{}-{}".format(PATENTS, n)
+            f = open(fname, "r")
+            if not f.readline().startswith("<?xml"):
+                print "You have not split the file {} in the correct boundary!".format(fname)
+            f.close()
+        except:
+            print "Could not find file {}. Check if the filename is correct!".format(fname)
+
+
+test()
