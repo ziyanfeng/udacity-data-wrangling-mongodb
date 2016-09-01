078484a1c2334249b61bea08204b863a9261cbca
ziyanfeng <ziyanfeng.c@gmail.com>
2016-09-01 19:11:57 -0400

    chore: correct directory name

--- Lesson 4 - Working with MongoDB/Using $in Operator/find_cars.py
+++ Lesson 4 - Working with MongoDB/Using $in Operator/find_cars.py
@@ -14,7 +14,7 @@ For instructions related to MongoDB setup and datasets please see Course Materia
 def in_query():
     # Modify the below line with your query; try to use the $in operator.
     query = {'manufacturer': 'Ford Motor Company', 'assembly': {'$in': ['Germany', 'United Kingdom', 'Japan']}}
-    
+
     return query
 
 
