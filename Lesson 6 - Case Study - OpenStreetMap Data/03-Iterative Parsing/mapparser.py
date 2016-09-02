43c7dc81fe0f368b1ceed929d6cc7eeb087bdf46
ziyanfeng <ziyanfeng.c@gmail.com>
2016-09-02 13:29:02 -0400

    feat: add solution to `Quiz: Iterative Parsing`

---
+++ Lesson 6 - Case Study - OpenStreetMap Data/03-Iterative Parsing/mapparser.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Your task is to use the iterative parsing to process the map file and
+find out not only what tags are there, but also how many, to get the
+feeling on how much of which data you can expect to have in the map.
+Fill out the count_tags function. It should return a dictionary with the
+tag name as the key and number of times this tag can be encountered in
+the map as value.
+
+Note that your code will be tested with a different data file than the 'example.osm'
+"""
+import xml.etree.ElementTree as ET
+import pprint
+
+
+def count_tags(filename):
+    # YOUR CODE HERE
+    tags = {}
+    for event, elem in ET.iterparse(filename):
+        if elem.tag not in tags:
+            tags[elem.tag] = 1
+        else:
+            tags[elem.tag] += 1
+    return tags
+
+
+def test():
+    tags = count_tags('example.osm')
+    pprint.pprint(tags)
+    assert tags == {'bounds': 1,
+                    'member': 3,
+                    'nd': 4,
+                    'node': 20,
+                    'osm': 1,
+                    'relation': 1,
+                    'tag': 7,
+                    'way': 1}
+
+
+if __name__ == "__main__":
+    test()
